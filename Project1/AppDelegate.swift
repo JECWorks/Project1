3d1444374321f473b449e1cc6af3dd2a0ae46768
Jason Cox <jasoncox@mac.com>
2020-05-21 12:40:24 -0400

    Initial Commit

---
+++ Project1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project1
+//
+//  Created by Jason Cox on 5/21/20.
+//  Copyright © 2020 Jason Cox. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
