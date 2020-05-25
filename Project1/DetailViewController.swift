0a90eafea3eaad79ceeae722fdea9c1342d45fbb
Jason Cox <jasoncox@mac.com>
2020-05-25 19:39:02 -0400

    Essentially the completed program in it's basic state. Next item would be to replace the NSTableView object with an Image and Text TableView and recreate the functionality of the current text only image titles.

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -16,4 +16,12 @@ class DetailViewController: NSViewController {
         // Do view setup here.
     }
     
+    /*
+     Every NSViewController has a parent property that points to the view controller its contained inside.
+     Split view controllers have a children property that is an array of the view controllers it contains. The second item in that array ought to be our DetailViewController.
+     We need to get the parent, and typecast it as a split view controller. If that works, we need to get its second child view controller and conditionally typecast it as a DetailViewController. If that works, we can call imageSelected(), passing in whatever image was tapped.
+     */
+    func imageSelected(name: String) {
+        imageView.image = NSImage(named: name)
+    }
 }
--- Project1/SourceViewController.swift
+++ Project1/SourceViewController.swift
@@ -11,16 +11,50 @@ import Cocoa
 class SourceViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
     // This makes the new class SourceViewController conform to all 3 protocols listed (NSViewController, etc.). This allows XCode to adjust it's autocomplete database to include things from these protocols as well.
     @IBOutlet var tableView: NSTableView!
+    
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        
+        // Get a list of all files in our bundles resource path:
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        // Loop over each file we found
+        
+        for item in items {
+            // if this item starts with "nssl" add it to our array
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
     }
     func numberOfRows(in tableView: NSTableView) -> Int {
-        return 100
+        return pictures.count
     }
     func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        // the viewFor method gets called once for every column and every row and returns the view shown in the slot.
+        // this guard statement changes (typecasts) the NSView request into a NSTableCellView request AND returns a nil
         guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
-        vw.textField?.stringValue = "Hello, World!"
+        vw.textField?.stringValue = pictures[row]
         return vw
+    }
+    // this asks the tableView what row is selected and protects by not selecting an image if no row is selected.
+    // This method gets called whenever the selected row gets changed. It reads the new selected row property of the talbe, ensuring it's not -1. Then it conditionally converts the source viewController's parent viewController into an NSSplitViewController (if this fails, it exits the method).
+    // Then it conditionally converts the splitVC second child view controller into a DetailViewController.
+    // If that works, then it calls the imageSelected method on it.
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        guard tableView.selectedRow != -1 else { return }
+        guard let splitVC = parent as? NSSplitViewController else { return }
+        
+        if let detail = splitVC.children[1] as? DetailViewController {
+            detail.imageSelected(name: pictures[tableView.selectedRow])
+        }
+        // It's necessary to change the content compression resistance to keep the new image from taking over the entire window.
+        // Content compression resistance means "how hard will I resist being made smaller than my content."
+        // Content Hugging Priority means "how hard will I resist being made bigger than my content."
     }
 }
