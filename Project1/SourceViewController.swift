de431ce3bcb256895a251562959cccb3cc9dfd79
Jason Cox <jasoncox@mac.com>
2020-05-25 23:30:10 -0400

    Branch which attempts to add thumbnails of the images from the original version.

--- Project1/SourceViewController.swift
+++ Project1/SourceViewController.swift
@@ -11,6 +11,7 @@ import Cocoa
 class SourceViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate {
     // This makes the new class SourceViewController conform to all 3 protocols listed (NSViewController, etc.). This allows XCode to adjust it's autocomplete database to include things from these protocols as well.
     @IBOutlet var tableView: NSTableView!
+   
     
     var pictures = [String]()
     
@@ -39,13 +40,16 @@ class SourceViewController: NSViewController, NSTableViewDataSource, NSTableView
         // the viewFor method gets called once for every column and every row and returns the view shown in the slot.
         // this guard statement changes (typecasts) the NSView request into a NSTableCellView request AND returns a nil
         guard let vw = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else { return nil }
+        vw.imageView?.image = NSImage.init(named: pictures[row])
         vw.textField?.stringValue = pictures[row]
         return vw
     }
+    
     // this asks the tableView what row is selected and protects by not selecting an image if no row is selected.
     // This method gets called whenever the selected row gets changed. It reads the new selected row property of the talbe, ensuring it's not -1. Then it conditionally converts the source viewController's parent viewController into an NSSplitViewController (if this fails, it exits the method).
     // Then it conditionally converts the splitVC second child view controller into a DetailViewController.
     // If that works, then it calls the imageSelected method on it.
+    
     func tableViewSelectionDidChange(_ notification: Notification) {
         guard tableView.selectedRow != -1 else { return }
         guard let splitVC = parent as? NSSplitViewController else { return }
